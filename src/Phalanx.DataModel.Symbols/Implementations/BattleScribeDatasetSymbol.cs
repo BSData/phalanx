ddd4d8063f5176fd25bfe7c6a06fdb46bd8d10e8
Amadeusz Sadowski <amis.tau.92@gmail.com>
2021-11-03 01:14:47 +0100

    feat: Add catgalogues in dataset symbol

--- src/Phalanx.DataModel.Symbols/Implementations/BattleScribeDatasetSymbol.cs
+++ src/Phalanx.DataModel.Symbols/Implementations/BattleScribeDatasetSymbol.cs
@@ -1,13 +1,38 @@
+using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
+using Phalanx.DataModel.Symbols.Binding;
+using WarHub.ArmouryModel.Source;
 
 namespace Phalanx.DataModel.Symbols.Implementation
 {
     public class BattleScribeDatasetSymbol : Symbol, IDatasetSymbol
     {
-        public BattleScribeDatasetSymbol()
+        public BattleScribeDatasetSymbol(ImmutableArray<CatalogueBaseNode> rootNodes)
         {
             // TODO
-            //Binder binder = null!; // TODO create binder
+            Binder binder = null!; // TODO create binder
+            BindingDiagnosticContext diagnostics = null!;
+            Catalogues = CreateCatalogues().ToImmutableArray();
+
+            IEnumerable<ICatalogueSymbol> CreateCatalogues()
+            {
+                foreach (var item in rootNodes)
+                {
+                    if (item is CatalogueNode catalogueNode)
+                    {
+                        yield return new CatalogueSymbol(this, catalogueNode, binder, diagnostics);
+                    }
+                    else if (item is GamesystemNode gamesystemNode)
+                    {
+                        yield return new GamesystemSymbol(this, gamesystemNode, binder, diagnostics);
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Unrecognized root node type.", nameof(rootNodes));
+                    }
+                }
+            }
         }
 
         public override SymbolKind Kind => SymbolKind.Catalogue;
