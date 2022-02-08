865e0ea08bb8bb3b1e1f9a2f6d425f37f9509cf0
Carl <heiberg.carl@gmail.com>
2022-02-08 23:22:34 +0100

    Added phalanx.app fiules removed bloat and using cdn for js and css packages

---
+++ src/Phalanx.App/Models/Faction.cs
@@ -0,0 +1,8 @@
+﻿namespace Phalanx.App.Models
+{
+    public class Faction
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ src/Phalanx.App/Models/OrgSlot.cs
@@ -0,0 +1,8 @@
+﻿namespace Phalanx.App.Models
+{
+    public class OrgSlot
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
