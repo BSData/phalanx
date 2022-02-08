c7f846f0b00fd416b59986e749a1ea756ffd274c
CarlHeiberg <heiberg.carl@gmail.com>
2022-02-08 23:26:21 +0100

    Added app project to phalanx sln (#23)
    * Added app project to phalanx sln
    
    * Added sample roster
    
    * Added phalanx.app files removed bloat and using cdn for js and css packages

---
+++ src/Phalanx.App/Models/Detachment.cs
@@ -0,0 +1,8 @@
+﻿namespace Phalanx.App.Models
+{
+    public class Detachment
+    {
+        public int id { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ src/Phalanx.App/Models/Unit.cs
@@ -0,0 +1,10 @@
+﻿namespace Phalanx.App.Models
+{
+    public class Unit
+    {
+        public int SubfactionId { get; set; }
+        public int OrgSlotId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int PointsPerModel { get; set; }
+    }
+}
