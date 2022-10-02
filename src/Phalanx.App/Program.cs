d35a2cb0e2e21bd30a3187b3c5ff696f7adb3105
Greg <gfitzpatrick2@gmail.com>
2022-10-02 12:57:47 -0400

    swapped font awesome for fluentui icons

--- src/Phalanx.App/Program.cs
+++ src/Phalanx.App/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Phalanx.App;
 using Phalanx.App.Pages.Printing;
+using Microsoft.Fast.Components.FluentUI;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<RosterFormatsProvider>();
+builder.Services.AddFluentUIComponents();
 
 await builder.Build().RunAsync();
