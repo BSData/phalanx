b761947db0cc4e5282124e272df1ee714685d025
jlueck6 <86534956+jlueck6@users.noreply.github.com>
2022-10-10 10:16:13 -0400

    feat: edit roster properties, fix add/remove selection - use ISymbols (#53)
    * Fixup roster Add. Edit cost values and roster names
    
    * Add said roster edit page
    
    * minor code cleanup
    
    * More add tinkering
    
    * Use symbols for add (...sort of), start sketching out Sel Edit
    
    * Use symbols just about everywhere. IOU 1 symbol-based Remove func
    
    * formatting
    
    * Updates from code review
    
    * style: apply 'dotnet format'
    
    * refactor: cleanup, format, fix code
    
    * fix: roster downloading
    
    * style: remove unnecessary space
    
    * style: cleanup
    
    * feat: get Add Selection operation working
    
    * style: more style and format cleanup
    
    Co-authored-by: Amadeusz Sadowski <amis.tau.92@gmail.com>

--- src/Phalanx.App/Program.cs
+++ src/Phalanx.App/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Phalanx.App;
 using Phalanx.App.Pages.Printing;
+using Phalanx.App.Util;
 using WarHub.ArmouryModel.DataProviders;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,11 +11,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<RosterFormatsProvider>();
-
-
+builder.Services.AddSingleton<RosterEditorService>();
 builder.Services.AddOptions<GalleryBrowserOptions>().BindConfiguration("GalleryBrowser");
 builder.Services.AddSingleton<GalleryBrowserState>();
 builder.Services.AddScoped<GalleryHttpClient>();
-
 
 await builder.Build().RunAsync();
